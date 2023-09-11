6df6b39da808df6b60a3722f96a6279754e09fa7
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-09-11 15:00:05 +0700

    Update Item.php

--- src/Item.php
+++ src/Item.php
@@ -79,6 +79,72 @@ class Item extends ItemCallback
             ['value' => 'col12', 'text' => 'Col12'],
         ];
     }
+    public static function getColors()
+    {
+        return [
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+
+            'blue',
+            'azure',
+            'indigo',
+            'purple',
+            'pink',
+            'red',
+            'orange',
+            'yellow',
+            'lime',
+            'green',
+            'teal',
+            'cyan',
+
+            'blue-lt',
+            'azure-lt',
+            'indigo-lt',
+            'purple-lt',
+            'pink-lt',
+            'red-lt',
+            'orange-lt',
+            'yellow-lt',
+            'lime-lt',
+            'green-lt',
+            'teal-lt',
+            'cyan-lt',
+
+            'gray-50',
+            'gray-100',
+            'gray-200',
+            'gray-300',
+            'gray-400',
+            'gray-500',
+            'gray-600',
+            'gray-700',
+            'gray-800',
+            'gray-900',
+
+            'facebook',
+            'twitter',
+            'linkedin',
+            'google',
+            'youtube',
+            'vimeo',
+            'dribbble',
+            'github',
+            'instagram',
+            'pinterest',
+            'vk',
+            'rss',
+            'flickr',
+            'bitbucket',
+            'tabler'
+        ];
+    }
     private function __construct($field)
     {
         $this->Field($field)
