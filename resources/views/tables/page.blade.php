88318ac750eb3f0b7bbe8f3522e49607d9ab22cd
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-09-16 01:16:00 +0700

    Update page.blade.php

--- resources/views/tables/page.blade.php
+++ resources/views/tables/page.blade.php
@@ -10,24 +10,33 @@
                 <div class="accordion" wire:ignore id="{{ $formSearchId }}-parent">
                     <div class="accordion-item">
                         <h2 class="accordion-header " id="{{ $formSearchId }}-header">
-                            <button class="accordion-button p-2 fs-2 ps-2  text-bg-blue bg-blue" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#{{ $formSearchId }}" aria-expanded="true"
+                            <button class="accordion-button p-2 fs-2 ps-2  text-bg-blue bg-blue" type="button"
+                                data-bs-toggle="collapse" data-bs-target="#{{ $formSearchId }}" aria-expanded="true"
                                 aria-controls="{{ $formSearchId }}">
                                 {{ $page_title }}
                             </button>
                         </h2>
                         <div id="{{ $formSearchId }}" class="accordion-collapse collapse show "
                             aria-labelledby="{{ $formSearchId }}-header" data-bs-parent="#{{ $formSearchId }}-parent">
-                            <div class="accordion-body p-2 bg-white" x-data="{ AdvancedSearch: false }">
-                                {!! form_render($itemManager->getFormSearch(), $formSearch, null) !!}
-                                <div style="display: none" x-show="AdvancedSearch">
-                                    Advanced Search
+                            <div class="accordion-body p-2 bg-white">
+                                <div class="row">
+                                    <div class="col">
+                                        {!! form_render($itemManager->getFormSearch(), $formSearch, null) !!}
+                                        <button class="btn btn-warning mt-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAdvancedSearch" aria-expanded="false" aria-controls="collapseAdvancedSearch">
+                                            Advanced Search </button>
+                                        <div class="collapse" wire:ignore id="collapseAdvancedSearch">
+                                            Advanced Search
+                                        </div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="text-end mb-2">
+                                            <a wire:click='doSearch()' class="btn btn-primary">Search</a>
+
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="text-end mb-2">
-                                    <button class="btn btn-warning" @click="AdvancedSearch=!AdvancedSearch">
-                                        Advanced Search </button>
-                                    <a wire:click='doSearch()' class="btn btn-primary">Search</a>
-                                </div>
+
+
 
                             </div>
                         </div>
