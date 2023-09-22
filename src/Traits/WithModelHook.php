ceab5b7409e1d9dd191705ffcc6fe0c5f9c6bee4
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-09-23 01:01:42 +0700

    Update WithModelHook.php

--- src/Traits/WithModelHook.php
+++ src/Traits/WithModelHook.php
@@ -7,11 +7,6 @@ trait WithModelHook
 {
     protected static function bootWithModelHook()
     {
-        static::creating(function ($model) {
-            do_action('BYTE_MODEL_BASE_CREATING', $model);
-        });
-        static::save(function ($model) {
-            do_action('BYTE_MODEL_BASE_CREATING', $model);
-        });
+       
     }
 }
