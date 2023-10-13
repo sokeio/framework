26178de5012b96b1c8b6de6d83fe70812b4ce2c0
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-10-13 13:50:01 +0700

    Update page.blade.php

--- resources/views/tables/page.blade.php
+++ resources/views/tables/page.blade.php
@@ -37,9 +37,6 @@
                                         </div>
                                     </div>
                                 </div>
-
-
-
                             </div>
                         </div>
                     </div>
