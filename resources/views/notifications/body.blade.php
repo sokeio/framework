edec00ee691ec4ebad607dbbf86525ff5a99dfcf
Dev Hau <50167033+devhau@users.noreply.github.com>
2024-01-05 14:16:56 +0700

    Update body.blade.php

--- resources/views/notifications/body.blade.php
+++ resources/views/notifications/body.blade.php
@@ -1,22 +1,31 @@
 @foreach ($notifications as $item)
-    <div class="row" x-data="{
+<div x-data="{
         isRead: {{ !count($item->userRead)? 'false' : 'true' }},
         TickRead() {
             this.isRead = true;
             $wire.TickRead('{{ $item->id }}');
         }
     }">
-        <div class="col-auto">
-            <span class="avatar" @click="TickRead()"></span>
-        </div>
-        <div class="col">
-            <div class="text-truncate">
-                {{ $item->title }}
+    @if($item->view)
+        @includeif($item->view,['item'=>$item])
+    @elseif($item->type)
+        @includeif($item->type,['item'=>$item])
+    @else
+        <div class="row" >
+            <div class="col-auto">
+                <span class="avatar" @click="TickRead()"></span>
             </div>
-            <div class="text-secondary">{{ $item->description }}</div>
+            <div class="col">
+                <div class="text-truncate">
+                    {{ $item->title }}
+                </div>
+                <div class="text-secondary">{{ $item->description }}</div>
+            </div>
+            <div class="col-auto align-self-center">
+                <div class="badge bg-primary" x-show="!isRead"></div>
+            </div>
         </div>
-        <div class="col-auto align-self-center">
-            <div class="badge bg-primary" x-show="!isRead"></div>
-        </div>
-    </div>
+    @endif
+</div>
+    
 @endforeach
