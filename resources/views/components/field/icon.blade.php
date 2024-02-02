3d4aea401850fef19ab12be5d35ab275dcaad61e
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-02-03 03:35:01 +0700

    Update icon.blade.php

--- resources/views/components/field/icon.blade.php
+++ resources/views/components/field/icon.blade.php
@@ -5,8 +5,11 @@
     $formField = $column->getFormField();
 @endphp
 <div>
-    <span x-show="$wire.{{ $formField }}" x-bind:class="$wire.{{ $formField }}" ></span>
-    <div class="form-text" x-text="$wire.{{ $formField }}"></div>
+    <p x-show="$wire.{{ $formField }}">
+        <span x-bind:class="$wire.{{ $formField }}"></span>
+        <span class="form-text" x-text="$wire.{{ $formField }}"></span><button class=" ms-3 btn btn-danger btn-sm"
+            @click="$wire.{{ $formField }}=''">Remove</button>
+    </p>
     <button class="btn btn-sm btn-primary" sokeio:form-setting="sokeio_icon_setting"
         sokeio:form-setting.model="{{ $formField }}">Change Icon</button>
 </div>
