25fed5acf963a4f868fbc72b57ce9cd426654ac0
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-02-03 03:38:41 +0700

    Create ColorSetting.php

---
+++ src/Livewire/ColorSetting.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sokeio\Livewire;
+
+use Sokeio\Components\FormSettingCallback;
+
+class ColorSetting extends FormSettingCallback
+{
+
+    protected function SettingUI()
+    {
+        return [];
+    }
+}
