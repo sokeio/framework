2eac741af255165c6964be5ff66701b7882aa96f
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-03-04 13:30:09 +0700

    Create WithLoadMore.php

---
+++ src/Concerns/WithLoadMore.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sokeio\Concerns;
+
+
+trait WithLoadMore
+{
+    public $dataItems = [];
+    public function getQuery(): mixed
+    {
+    }
+    public function getPageSize()
+    {
+        return 12;
+    }
+    public $page = 0;
+    public function loadMore()
+    {
+        $this->page++;
+        $query = $this->getQuery();
+        $items = $query->paginate($this->getPageSize(), ['*'], 'page', $this->page)->items();
+        if (count($items) == 0) {
+            $this->page--;
+            return;
+        }
+        $this->dataItems = array_merge($this->dataItems, $items);
+        return $this->dataItems;
+    }
+    public function getLoadMoreHtml($text = 'Loading..')
+    {
+        return '<div wire:loading class="text-center p-2">' . $text . '</div>
+    <div class="p-4" x-intersect.full="$wire.loadMore()"></div> ';
+    }
+}
