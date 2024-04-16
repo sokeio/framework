74c74cb92b069aadd3cb3cfa7c09ee3d2778f375
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-04-16 14:07:15 +0700

    Update updater.blade.php

--- resources/views/updater.blade.php
+++ resources/views/updater.blade.php
@@ -3,6 +3,7 @@
         <div class="card rounded-1" style="max-width: 700px;">
             <h5 class="card-header p-3">@lang('System Updater')</h5>
             <div class="card-body p-3">
+                <div class="mb-3"> This feature not available on version beta.</div>
                 <div class="text-center">
                     <button class="btn btn-primary" wire:click='doUpdate()'>@lang('Update System')</button>
                 </div>
