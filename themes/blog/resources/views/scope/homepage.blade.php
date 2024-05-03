f5c0f4d61470bbc4aa0b3853d65f5e726ebafcb7
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-05-04 01:39:19 +0700

    Update homepage.blade.php

--- themes/blog/resources/views/scope/homepage.blade.php
+++ themes/blog/resources/views/scope/homepage.blade.php
@@ -1,4 +1,4 @@
 <div class="container">
-    <div class="mt-4 text-center" wire:qrcode wire:qrcode.title="Sokeio" wire:qrcode.text="https://hau.xyz">
+    <div class="mt-4 text-center" wire:qrcode wire:qrcode.title="Sokeio Framework" wire:qrcode.text="https://hau.xyz">
     </div>
 </div>
