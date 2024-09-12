1ff60daf85599abf1540510ec6fa4ca26ca391b9
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-09-12 09:05:54 +0700

    update code

--- resources/js/framework/common/Uitls.js
+++ resources/js/framework/common/Uitls.js
@@ -15,16 +15,28 @@ const tagSplit = "############$$$$$$$$############";
 function LOG(...args) {
   console.log(...args);
 }
+function mapObservableProxy(app) {
+  let obj = {};
+  Object.keys(app.state).forEach((key) => {
+    // Định nghĩa getter và setter cho thuộc tính value
+    Object.defineProperty(obj, key, {
+      get() {
+        return app[key]; // Trả về giá trị của _value
+      },
+      set(newValue) {
+        app[key] = newValue;
+      },
+      enumerable: true, // Để thuộc tính có thể được liệt kê
+      configurable: true, // Cho phép cấu hình lại thuộc tính
+    });
+  });
+  return obj;
+}
 function runFunction(txtFunc, $event, app) {
-  return new Function(
-    "$event",
-    `
- return  () => {
-     ${txtFunc}
-     console.log($event);
-  }
-     `
-  ).apply(app, [$event])();
+  return new Function("$event", "$obj", `with($obj) {  ${txtFunc}}`).apply(
+    app,
+    [$event, mapObservableProxy(app)]
+  );
 }
 export const Utils = {
   getComponentsFromText,
