e3f232ee8a70bbfe09f403fd2be1f57924e06422
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-11-13 00:28:34 +0700

    update code

--- resources/js/file-mananger/index.js
+++ resources/js/file-mananger/index.js
@@ -9,8 +9,8 @@ window.showFileManager = function (callback, type = "file") {
     skipOverlayClose: true,
   });
 };
-setTimeout(() => {
-  window.showFileManager(function (file) {
-    console.log(file);
-  });
-}, 100);
+// setTimeout(() => {
+//   window.showFileManager(function (file) {
+//     console.log(file);
+//   });
+// }, 100);
