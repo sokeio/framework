a5e9012bc366be11db8571fd8b560675e9657e09
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-11-29 23:00:47 +0700

    Update Card.php

--- src/UI/Common/Card.php
+++ src/UI/Common/Card.php
@@ -34,23 +34,41 @@ class Card extends BaseUI
     {
         return $this->vars('column', $column);
     }
+    private function subtitleRender()
+    {
+        if ($this->checkVar('subtitle')) {
+            return <<<HTML
+            <p class="text-secondary">{$this->getVar('subtitle', '', true)}</p>
+            HTML;
+        }
+        return '';
+    }
+    private function titleRender()
+    {
+        if ($this->checkVar('title')) {
+            return <<<HTML
+            <h3 class="card-title">{$this->getVar('title', '', true)}</h3>
+            HTML;
+        }
+        return '';
+    }
     public function view()
     {
         $attr = $this->getAttr();
         $html = <<<HTML
-        <div {$attr}>
-        <div class="card-body {$this->getVar('bodyClass', 'p-3', true)}">
-        <h3 class="card-title">{$this->getVar('title', '', true)}</h3>
-        <p class="text-secondary">{$this->getVar('subtitle', '', true)}</p>
-        </div>
-        {$this->getVar('text', '', true)}{$this->renderChilds()}
-    </div>
-    HTML;
+            <div {$attr}>
+                <div class="card-body {$this->getVar('bodyClass', 'p-3', true)}">
+                    {$this->titleRender()}
+                    {$this->subtitleRender()}
+                    {$this->getVar('text', '', true)}{$this->renderChilds()}
+                </div>
+            </div>
+        HTML;
         if ($this->getVar('column', '', true)) {
             $html = <<<HTML
-        <div class="{$this->getVar('column', '', true)}">
-            {$html}
-        </div>
+            <div class="{$this->getVar('column', '', true)}">
+                {$html}
+            </div>
         HTML;
         }
         return $html;
