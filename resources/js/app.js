0bbdb15a6113b80a8c9cf7b0201de1208f8e8a7a
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-12-03 02:48:23 +0700

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,9 @@ import "./file-mananger";
 import "./modal/index";
 import "./livewire";
 import "./alpine";
+import { addScriptToWindow, addStyleToWindow } from "./utils";
+
+window.addStyleToWindow = addStyleToWindow;
+window.addScriptToWindow = addScriptToWindow;
 
 window.sokeioUI = SokeioUI;
