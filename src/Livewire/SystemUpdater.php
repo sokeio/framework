c756c9efac34328a354ced933a4c04e6bb6c12ff
nguyen van hau <nguyenvanhau.soft@gmail.com>
2025-01-03 16:36:30 +0700

    Update SystemUpdater.php

--- src/Livewire/SystemUpdater.php
+++ src/Livewire/SystemUpdater.php
@@ -11,6 +11,25 @@ class SystemUpdater extends Component
 {
     public $isSystemVersionNew = false;
     public $productInfo = [];
+    public $start = 3;
+ 
+    public function begin()
+    {
+        while ($this->start >= 0) {
+            // Stream the current count to the browser...
+            $this->stream(  
+                to: 'count',
+                content: $this->start,
+                replace: true,
+            );
+ 
+            // Pause for 1 second between numbers...
+            sleep(1);
+ 
+            // Decrement the counter...
+            $this->start = $this->start - 1;
+        };
+    }
     public function checkSystemUpdate()
     {
         return Marketplate::checkNewVersion();
